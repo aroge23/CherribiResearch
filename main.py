b92c6e5cd4996c19bb328fad06cedc753ad8ab30
aroge23 <42726350+aroge23@users.noreply.github.com>
2022-09-27 11:07:40 -0400

    added simple json view

--- main.py
+++ main.py
@@ -1,31 +1,40 @@
 from youtubesearchpython import VideosSearch
 from youtube_transcript_api import YouTubeTranscriptApi as ytapi
+import json as js
 
 videoSearch = VideosSearch("jahiliyyah", limit= 5)
 searches = videoSearch.result()['result']
 
 json = {}
 
-for search in searches:
-    title = search['title']
+for search in searches[1:2]:
+    print(js.dumps(search, indent=4))
 
-    searchjson = {}
+    # Time range between 2015-2019
+    published = search['publishedTime']
+    yearsAgo = int(published[:2])
+    if (yearsAgo > 7 or yearsAgo < 3):
+        print(published)
+    else:
+        title = search['title']
 
-    id = search['id']
-    searchjson['id'] = id
+        searchjson = {}
 
-    url = search['link']
-    searchjson['url'] = url
+        id = search['id']
+        searchjson['id'] = id
 
-    try:
-        texts = ytapi.get_transcript(id, languages=['en'])
-        transcript = []
-        for text in texts:
-            transcript.append(text['text'])
-        searchjson['text'] = transcript
-    except Exception as e:
-        searchjson['text'] = transcript
-        print('Error for title "{}": '.format(title), e)
-    json[title] = searchjson
+        url = search['link']
+        searchjson['url'] = url
 
-print(json)
+        try:
+            texts = ytapi.get_transcript(id, languages=['en'])
+            transcript = []
+            for text in texts:
+                transcript.append(text['text'])
+            searchjson['text'] = transcript
+        except Exception as e:
+            searchjson['text'] = transcript
+            print('Error for title "{}": '.format(title), e)
+        json[title] = searchjson
+
+# print(next(iter(json.items())))
