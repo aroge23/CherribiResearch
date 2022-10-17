90da070320bbfaaeae7730dbe11de9b5ade3fcd6
aroge23 <42726350+aroge23@users.noreply.github.com>
2022-10-16 21:53:56 -0400

    added channel_id

--- main.py
+++ main.py
@@ -1,24 +1,32 @@
-from youtubesearchpython import VideosSearch
+from youtubesearchpython import *
 from youtube_transcript_api import YouTubeTranscriptApi as ytapi
 import json as js
+import csv
 
-query = "jahiliyyah"
-videoSearch = VideosSearch(query, limit= 5)
+query = "jihad"
+videoSearch = VideosSearch(query, language='ur', limit=40) #try language='ur, ar, hi, pa'
 searches = videoSearch.result()['result']
-
+# print(js.dumps(searches[0], indent=4))
 json = {}
+searchList = []
+
 
 for search in searches:
-
+    # print(js.dumps(search, indent=4))
     # Time range between 2015-2019
     published = search['publishedTime']
-    yearsAgo = int(published[:2])
-    if (yearsAgo > 7 or yearsAgo < 3):
-        print('The video {} was published {}'.format(search['title'], published))
-    else:
-        title = search['title']
-
+    try:
+        yearsAgo = int(published[:2])
+        # if (yearsAgo > 7 or yearsAgo < 3):
+        #     print('The video {} was published {}'.format(search['title'], published))
+        # else:
         searchjson = {}
+
+        title = search['title']
+        searchjson['title'] = title
+
+        channel_id = search['channel']['id']
+        searchjson['channel_id'] = channel_id
 
         id = search['id']
         searchjson['id'] = id
@@ -36,6 +44,21 @@ for search in searches:
             searchjson['text'] = transcript
             print('Error for title "{}": '.format(title), e)
         json[title] = searchjson
+        searchList.append(searchjson)
+    except Exception as e:
+        print('Error on search {}: {}'.format(search, e))
 
 # print(next(iter(json.items())))
-print(js.dumps(json, indent=4))
+# print(js.dumps(json, indent=4))
+print(js.dumps(searchList, indent=4))
+
+# transcripts = open('transcripts.csv', 'w')
+# csv_writer = csv.writer(transcripts)
+
+# count = 0
+# for key in searchList:
+#     if count == 0:
+#         header = key.keys()
+#         csv_writer.writerow(header)
+#         count += 1
+#     csv_writer.writerow(key.values())
